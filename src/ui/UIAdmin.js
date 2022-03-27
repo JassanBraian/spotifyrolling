f241797cd8d6b070270094257438eb3e867b9d27
Jorge Gonzalez <Jdgonzalez31@gmail.com>
2022-03-27 20:46:44 -0300

    comentario de aclaracion

--- src/ui/UIAdmin.js
+++ src/ui/UIAdmin.js
@@ -1,13 +1,15 @@
-import UIAlbum from './UIAlbum.js';
+import UIAlbum from "./UIAlbum.js";
 
-const uiAlbum = new UIAlbum();
+const uiAlbum = new UIAlbum(); //instanciando variable de UIAlbum
 
-const tbodyAlbumList = document.querySelector('#tableAlbumList').getElementsByTagName('tbody')[0];
+const tbodyAlbumList = document
+  .querySelector("#tableAlbumList")
+  .getElementsByTagName("tbody")[0];
 
 addListeners();
 
 function addListeners() {
-    document.addEventListener("DOMContentLoaded", async e => {
-        tbodyAlbumList.append(...await uiAlbum.buildAlbumList());
-    });
+  document.addEventListener("DOMContentLoaded", async (e) => {
+    tbodyAlbumList.append(...(await uiAlbum.buildAlbumList()));
+  });
 }
