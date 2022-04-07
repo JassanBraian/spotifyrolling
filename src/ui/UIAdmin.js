d2810713fc217e0234fdda1dcd5912ed8bef13cb
Braian <braianjassan@gmail.com>
2022-04-07 03:35:32 -0300

    admin rol user validation was added before load admin.html screen

--- src/ui/UIAdmin.js
+++ src/ui/UIAdmin.js
@@ -1,6 +1,8 @@
 import UIAlbum from './UIAlbum.js';
+import UserServices from '../utils/UserServices.js';
 
 const uiAlbum = new UIAlbum();
+const userServices = new UserServices();
 
 const tbodyAlbumList = document.querySelector('#tableAlbumList').getElementsByTagName('tbody')[0];
 
@@ -8,6 +10,19 @@ addListenersAdmin();
 
 function addListenersAdmin() {
     document.addEventListener("DOMContentLoaded", async function (e) {
+
+        //localStorage.setItem('userLogged', JSON.stringify({ nombreUsuario: 'emanuelRojas' }));
+        // const userLogged = JSON.parse(localStorage.getItem('userLogged'))
+        // if (userLogged && userLogged.nombreUsuario) {
+        //     const userDB = await userServices.getUserByName(userLogged.nombreUsuario);
+        //     if (!(await userDB) || await userDB.rol !== 'admin') {
+        //         window.location.href = "index.html";
+        //     }
+        // } else window.location.href = "index.html";
+
+        await userServices.validateUserLoggedRolAdmin() ? true : 
+            window.location.href = "index.html";
+
         tbodyAlbumList.append(...await uiAlbum.buildAlbumList());
         document.body.append(await uiAlbum.buildAlbumModalCrud());
         document.body.append(await
@@ -15,6 +30,7 @@ function addListenersAdmin() {
                 'Seguro desea borrar el Album?', 'btnModalConfirmDelete'));
 
         uiAlbum.addListenersAlbum();
+
     });
 
 }
