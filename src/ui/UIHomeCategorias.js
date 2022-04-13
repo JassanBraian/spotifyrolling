2d217177b0c87d842a280d60456f5245b9636a8f
ElisaSoco <esocolsky@gmail.com>
2022-04-13 19:19:56 -0300

    imagenes e inicio de UI categorias

--- src/ui/UIHomeCategorias.js
+++ src/ui/UIHomeCategorias.js
@@ -1,8 +1,66 @@
- import AlbumServices from "../utils/AlbumServicesMockup.js";
-// const albumServices = new AlbumServices();
-// const albumes = await getAlbumes();
-// const bodyHTML = document.getElementsByTagName('body');
-// bodyHTML.append(albumes);
-// console.log(albumes);
+window.onload = function () {
+    // Variables
+    const IMAGENES = [
+        'docs/img/rock1.jpg',
+        'docs/img/rock2.jpg',
+        'docs/img/rock3.jpg',
+        'docs/img/rock4.jpg'
+    ];
+    
+    let posicionActual = 0;
+    let $botonRetroceder = document.querySelector('#retroceder');
+    let $botonAvanzar = document.querySelector('#avanzar');
+    let $imagen = document.querySelector('#imagen');
+    
+    
 
-console.log('hola')
+    // Funciones
+
+    /**
+     * Funcion que cambia la foto en la siguiente posicion
+     */
+    function pasarFoto() {
+        if(posicionActual >= IMAGENES.length - 1) {
+            posicionActual = 0;
+        } else {
+            posicionActual++;
+        }
+        renderizarImagen();
+    }
+
+    /**
+     * Funcion que cambia la foto en la anterior posicion
+     */
+    function retrocederFoto() {
+        if(posicionActual <= 0) {
+            posicionActual = IMAGENES.length - 1;
+        } else {
+            posicionActual--;
+        }
+        renderizarImagen();
+    }
+
+    /**
+     * Funcion que actualiza la imagen de imagen dependiendo de posicionActual
+     */
+    function renderizarImagen () {
+        $imagen.style.backgroundImage = `url(${IMAGENES[posicionActual]})`;
+    }
+
+    /**
+     * Activa el autoplay de la imagen
+     */
+    
+
+    /**
+     * Para el autoplay de la imagen
+     */
+    
+
+    // Eventos
+    $botonAvanzar.addEventListener('click', pasarFoto);
+    $botonRetroceder.addEventListener('click', retrocederFoto);
+    
+    // Iniciar
+    renderizarImagen();
+} 
