a7143b3c70fee1cd0e7ba23a0490ed53e28889f9
ElisaSoco <esocolsky@gmail.com>
2022-04-14 19:26:22 -0300

    primer carrousel armado funcionando con start y stop

--- src/ui/UIHomeCategorias.js
+++ src/ui/UIHomeCategorias.js
@@ -3,6 +3,41 @@ const albumServices = new AlbumServices();
 const albumsPop = await albumServices.getAlbumsByCategory('rock');
 const albumsDestacados = await albumServices.getAlbumsDestacados();
 
+
+
+//Inicio Carrousel
+const carrousel = document.querySelector('.carrousel-items');
+let maxScrollLeft = carrousel.scrollWidth - carrousel.clientWidth;
+let interval = null;
+let step = 1;
+
+
+const start = ()=>{
+    interval = setInterval(function () {
+        carrousel.scrollLeft = carrousel.scrollLeft + step;
+        if (carrousel.scrollLeft === maxScrollLeft){
+            step = step * -1;
+        } else if(carrousel.scrollLeft === 0){
+            step = step * -1;
+        };
+    }, 10);
+};
+
+const stop = ()=>{
+    clearInterval(interval);
+};
+
+
+carrousel.addEventListener('mouseover', ()=>{
+    stop();
+} );
+carrousel.addEventListener('mouseout', ()=>{
+    start();
+});
+
+start();
+//fin carrousel
+
 console.log(albumsPop);
 console.log(albumsDestacados);
 
