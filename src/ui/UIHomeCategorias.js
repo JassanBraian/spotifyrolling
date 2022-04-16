9bec5e954a406bcb19895f12cedcffa0012f302a
ElisaSoco <esocolsky@gmail.com>
2022-04-15 21:13:13 -0300

    index con categorias sin carrousel con estilos sin imagenes def

--- src/ui/UIHomeCategorias.js
+++ src/ui/UIHomeCategorias.js
@@ -1,41 +1,70 @@
 import AlbumServices from '../utils/AlbumServices.js'
 const albumServices = new AlbumServices();
-const albumsPop = await albumServices.getAlbumsByCategory('rock');
 const albumsDestacados = await albumServices.getAlbumsDestacados();
+const albumsPop = await albumServices.getAlbumsByCategory('pop');
+const albumsRock = await albumServices.getAlbumsByCategory('rock');
+const albumsLatinos = await albumServices.getAlbumsByCategory('latino');
+const carrouselPop = document.querySelector('#carrousel-pop');
+const carrouselRock = document.querySelector('#carrousel-rock');
+const carrouselLatino = document.querySelector('#carrousel-latino');
+const carrouselDestacados = document.querySelector('#carrousel-destacados')
+
+function addListeners() {
+    document.addEventListener("DOMContentLoaded", async e => {
+        
+    })};
+    
+    function createCarrousel(albums, carrousel){
+        albums.forEach(album =>{
+            const carrouselItems = document.createElement('div');
+            carrouselItems.classList.add('carrousel-items','col-sm-4');
+            carrouselItems.innerHTML =`
+            <img src=${album.imgUrl}>
+            <div class = "text">${album.nombre}-${album.artista}</div>
+            `
+            carrousel.append(carrouselItems);
+        });
+        
+    };
+createCarrousel(albumsPop, carrouselPop);
+createCarrousel(albumsRock, carrouselRock);
+createCarrousel(albumsLatinos, carrouselLatino);
+createCarrousel(albumsDestacados, carrouselDestacados);
 
 
 
 //Inicio Carrousel
-const carrousel = document.querySelector('.carrousel-items');
-let maxScrollLeft = carrousel.scrollWidth - carrousel.clientWidth;
-let interval = null;
-let step = 1;
+// const carrousel = document.querySelector('#carrousel-pop');
+// let maxScrollLeft = carrousel.scrollWidth - carrousel.clientWidth;
+// let interval = null;
+// let step = 1;
 
 
-const start = ()=>{
-    interval = setInterval(function () {
-        carrousel.scrollLeft = carrousel.scrollLeft + step;
-        if (carrousel.scrollLeft === maxScrollLeft){
-            step = step * -1;
-        } else if(carrousel.scrollLeft === 0){
-            step = step * -1;
-        };
-    }, 10);
-};
+// const start = ()=>{
+//     interval = setInterval(function () {
+//         carrousel.scrollLeft = carrousel.scrollLeft + step;
+//         if (carrousel.scrollLeft === maxScrollLeft){
+//            step = step * -1;
+//         } else if(carrousel.scrollLeft === 0){
+//             step = step * -1;
+//         };
+//     }, 10);
+// };
 
-const stop = ()=>{
-    clearInterval(interval);
-};
+// const stop = ()=>{
+//     clearInterval(interval);
+// };
 
 
-carrousel.addEventListener('mouseover', ()=>{
-    stop();
-} );
-carrousel.addEventListener('mouseout', ()=>{
-    start();
-});
+// carrousel.addEventListener('mouseover', ()=>{
+//     stop();
+// } );
+// carrousel.addEventListener('mouseout', ()=>{
+//     start();
+// });
 
-start();
+// start();
+
 //fin carrousel
 
 console.log(albumsPop);
