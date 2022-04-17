e39aed913eb60a55109c887fb4d32d1027302a1a
Alexis <vale.alexis18@gmail.com>
2022-04-17 17:49:18 -0300

    agregado de seccion comentarios y reduccion de codigo en login

--- src/ui/login.js
+++ src/ui/login.js
@@ -1,14 +1,15 @@
 //variables
 const usuarioLogin = document.getElementById("usuarioLogin");
 const passwordLogin = document.getElementById("passwordLogin");
-const loginErrorText=document.querySelector(".grupo_usuarioLogin p")
+const loginErrorUser= document.querySelector(".grupo_usuarioLogin p");
+const loginErrorPass= document.querySelector(".grupo_passwordLogin p");
+const buttonIniciar= document.querySelector(".form_login button");
 let usuarioCorrecto = false;
 
 //definimos los listening
 form_login.addEventListener("submit", validarEntradas);
-//defino los campos and uso otro Admin
+
 const campoLogin = {
-  //ESTA PARTE CREO QUE NO ES NECESARIO
   usuario: false,
   password: false
 };
@@ -16,13 +17,13 @@ function verificar(entrada) {
   if (entrada.id == "usuarioLogin") {
     if (expressions.usuario.test(entrada.value)) {
       campoLogin["usuario"] = true;
-      loginErrorText.classList.remove("activeMsj");
+      loginErrorUser.classList.remove("activeMsj");
       usuarioLogin.classList.remove("border_input_none","border_color_red");
       usuarioLogin.classList.add("border_color_green");
       viewError("usuarioLogin", true);
     } else {
       campoLogin["usuario"] = false;
-      loginErrorText.classList.add("activeMsj");
+      loginErrorUser.classList.add("activeMsj");
       usuarioLogin.classList.remove("border_input_none","border_color_green");
       usuarioLogin.classList.add("border_color_red");
       viewError("usuarioLogin", false);
@@ -31,16 +32,14 @@ function verificar(entrada) {
   if (entrada.id == "passwordLogin") {
     if (expressions.password.test(entrada.value)) {
       campoLogin["password"] = true;
-      loginErrorText.classList.remove("activeMsj");
+      loginErrorPass.classList.remove("activeMsj");
       passwordLogin.classList.remove("border_input_none");
       passwordLogin.classList.remove("border_color_red");
       passwordLogin.classList.add("border_color_green");
       viewError("passwordLogin", true);
     } else {
       campoLogin["password"] = false;
-      document
-        .querySelector(".grupo_passwordLogin p")
-        .classList.add("activeMsj");
+      loginErrorPass.classList.add("activeMsj");
       passwordLogin.classList.remove("border_input_none");
       passwordLogin.classList.remove("border_color_green");
       passwordLogin.classList.add("border_color_red");
@@ -53,27 +52,13 @@ const viewError = async (campo, valor) => {
   try {
     if (valor) {
       setTimeout(() => {
-        document
-          .querySelector(`.grupo_${campo} input`)
-          .classList.remove("border_color_red");
-        document
-          .querySelector(`.grupo_${campo} input`)
-          .classList.remove("border_color_green");
-        document
-          .querySelector(`.grupo_${campo} input`)
-          .classList.add("border_input_none");
+        document.querySelector(`.grupo_${campo} input`).classList.remove("border_color_red","border_color_green");
+        document.querySelector(`.grupo_${campo} input`).classList.add("border_input_none");
       }, 3000);
     } else {
       setTimeout(() => {
-        document
-          .querySelector(`.grupo_${campo} input`)
-          .classList.remove("border_color_green");
-        document
-          .querySelector(`.grupo_${campo} input`)
-          .classList.remove("border_color_red");
-        document
-          .querySelector(`.grupo_${campo} input`)
-          .classList.add("border_input_none");
+        document.querySelector(`.grupo_${campo} input`).classList.remove("border_color_green","border_color_red");
+        document.querySelector(`.grupo_${campo} input`).classList.add("border_input_none");
       }, 3000);
     }
   } catch (error) {
@@ -82,9 +67,9 @@ const viewError = async (campo, valor) => {
 };
 function habilitarBtnIniciar() {
   if (campoLogin.usuario && campoLogin.password) {
-    document.querySelector(".form_login button").disabled = false;
+    buttonIniciar.disabled = false;
   } else {
-    document.querySelector(".form_login button").disable = true;
+    buttonIniciar.disable = true;
   }
 }
 
